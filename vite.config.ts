c4dab6488db4901774dadb61fe2f221aa03361a4
余磊 <yulei@addcn.com>
2020-12-04 09:23:36 +0800

    chore: change assetsDir

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ const path = require('path')
 
 module.exports = {
     base: './',
+    assetsDir: 'assets',
     esbuildTarget: 'es2015',
     alias: {
         '/@/': path.resolve(__dirname, './src')
