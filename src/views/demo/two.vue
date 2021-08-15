4c2ab2e5a706ac924f9be65ad6430aac52a61098
余磊 <yulei@addcn.com>
2021-08-15 15:08:55 +0800

    chore(app): update to vite2

---
+++ src/views/demo/two.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>《前端开发核心知识进阶》- 获取元素至文档顶部的距离</h2>
+        <div class="box"></div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    setup () {
+
+    }
+})
+</script>
+
+<style scoped>
+    .box {
+        width: 200px;
+        height: 200px;
+        border: 1px solid #bcbcbc;
+        background-color: cadetblue;
+    }
+</style>
