7dcbf90de50f94e712aa121c05adab4053c4fa4e
余磊 <yulei@addcn.com>
2022-03-05 21:41:24 +0800

    chore(build): add unocss

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router/'
 import { store } from './store/'
 import './index.css'
+import 'uno.css'
 
 createApp(App).use(router).use(store).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,27 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
+import presetAttributify from '@unocss/preset-attributify'
 
 export default defineConfig({
   base: '/poison-lab/',
-  resolve:{
-      alias: {
-      '/@': path.resolve(__dirname, './src')
-  }},
+  resolve: {
+    alias: {
+      '/@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
-    port: 3001
+    port: 3001,
   },
   plugins: [
-    vue()
-  ]
+    vue(),
+    Unocss({
+      presets: [presetAttributify()],
+      rules: [
+        ['input-default', { padding: 0, height: '30px', 'min-width': '500px' }],
+        ['btn-default', { height: '32px', 'min-width': '100px', 'background-color': '#fff', border: '1px solid #ccc' }],
+      ],
+    }),
+  ],
 })
